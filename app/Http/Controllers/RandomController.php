704e0f89155d9234c43fd40265e3b4b6688e0315
Yannick Kreuter <y.kreuterle@sbg.at>
2026-03-06 10:40:44 +0100

    add multiple placeholders

--- app/Http/Controllers/RandomController.php
+++ app/Http/Controllers/RandomController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Nette\Utils\Random;
 
 class RandomController extends Controller
 {
     public function select(){
-        return random_int(1, 9);
+
+            // return random_int(1,9);
+
+            $stud = random_int(1,9);
+            return view('students.random', [
+            'stud' => $stud
+        ]);
+
     }
 }
---
+++ resources/views/students/random.blade.php
@@ -0,0 +1,5 @@
+<h2>{{ __('Schüler:in ausgewhählt')}}</h2>
+
+<p>{{ __('Selected: :student', ['student' => $stud]) }}</p>
+
+<p>{{ __(':firstname reached :points points', ['firstname' => 'Josef', 'points' => 5]) }}</p>
